c76b62a4b9056a43df97ee4842f842e77252a2e6
Mario Leyva Moreno <leyvam526@gmail.com>
2023-05-13 00:21:03 -0700

    added custom server-side validation for password confirmation

---
+++ application/public/js/frontend.js
@@ -0,0 +1,17 @@
+const form = document.getElementById('form');
+// Get the password and re-enter password fields
+const rePasswordField = document.getElementById('re-password');
+const passwordField = document.getElementById('password');
+const invalidFeedback = rePasswordField.nextElementSibling;
+form.addEventListener('submit', validatePassword);
+
+function validatePassword(event) {
+    event.preventDefault();
+    // Add an event listener to the re-enter password field
+    if (rePasswordField.value !== passwordField.value) {
+        event.preventDefault();
+        invalidFeedback.textContent = 'Password does not match.';
+    } else {
+        invalidFeedback.textContent = 'Passwords Match!';
+    }
+}
