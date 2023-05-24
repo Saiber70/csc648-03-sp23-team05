97e2e83bb6f51ab6c0688d8f896fb140b8da149c
Mario Leyva Moreno <leyvam526@gmail.com>
2023-05-24 11:14:21 -0700

    added more comments and deleted old files

--- application/public/js/password_validation.js
+++ application/public/js/password_validation.js
@@ -1,3 +1,12 @@
+/**************************************************************
+* Author: Mario Leyva Moreno
+*
+* File: password_validation.js
+* Description: The purpose of this file is to set up server-side validation for password
+* confirmation. Current code is not working properly.
+*
+**************************************************************/
+
 const form = document.getElementById('form');
 // Get the password and re-enter password fields
 const rePasswordField = document.getElementById('re-password');
